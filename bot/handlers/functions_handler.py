935c92ea0795bb6e1e38cee974b95e26ef752b8e
muhammadnur <muhammadnurpdp@gmail.com>
2024-06-22 18:46:19 +0500

    All updates

--- bot/handlers/functions_handler.py
+++ bot/handlers/functions_handler.py
@@ -23,7 +23,9 @@ business_card_id = -1002196387423
 async def sell_function(msg: types.Message, state: FSMContext):
     await state.set_state("sell")
     if msg.text == sell:
-        await msg.answer(text="""   Bu yerda siz:
+        await msg.answer(text="""
+    
+    Bu yerda siz:
     Xo'jalik mollari
     Qurilish mahsulotlari 
     Maishiy texnikalar
@@ -34,7 +36,9 @@ async def sell_function(msg: types.Message, state: FSMContext):
     Tovarlarni sotish jarayonida har ikki tomondan halollik va savdo qonun-qoidalarga bo'ysunish talab etiladi""",
                          reply_markup=await back_main_menu_button(msg.from_user.id))
     else:
-        await msg.answer(text="""   Вот ты:
+        await msg.answer(text="""
+    
+    Вот ты:
     Домашние товары
     Строительная продукция 
     Бытовая техника
@@ -76,12 +80,16 @@ Telefon raqam: {tg_user['phone_number']}"""
 async def buy_function(msg: types.Message, state: FSMContext):
     await state.set_state("buy")
     if msg.text == buy:
-        await msg.answer(text="""   Bu yerdan siz yangi xo'jalik va qurilish mollarini, turli xildagi aksessuarlarni xarid qilishingiz mumkin. 
+        await msg.answer(text="""
+        
+    Bu yerdan siz yangi xo'jalik va qurilish mollarini, turli xildagi aksessuarlarni xarid qilishingiz mumkin. 
     Buning uchun sizdan mahsulot nomi yoki suratini ilova qilgan holda izlash talab etiladi
     Bot orqali xarid qilish jarayonida har ikki tomondan halollik va savdo qonun-qoidalariga rioya qilish talab etiladi. Mahsulot sotib olayotganda sotuvchidan chek talab qilishni unutmang!""",
                          reply_markup=await back_main_menu_button(msg.from_user.id))
     else:
-        await msg.answer(text="""   Здесь вы можете купить новые товары для дома и строительства, различные аксессуары. 
+        await msg.answer(text="""
+    
+    Здесь вы можете купить новые товары для дома и строительства, различные аксессуары. 
     Для этого попросите выполнить поиск по названию продукта или прикрепленному изображению.
     Требуйте честности и справедливой торговой практики от обеих сторон процесса совершения покупок с помощью ботов. При покупке товара запросите у продавца чек!""",
                          reply_markup=await back_main_menu_button(msg.from_user.id))
@@ -117,7 +125,9 @@ Telefon raqam: {tg_user['phone_number']}"""
 async def offer_function(msg: types.Message, state: FSMContext):
     await state.set_state("offer")
     if msg.text == offer:
-        await msg.answer(text="""   Bu yerda siz o'z takliflaringizni qoldirishingiz mumkin. 
+        await msg.answer(text="""
+    
+    Bu yerda siz o'z takliflaringizni qoldirishingiz mumkin. 
     Bot foydalanuvchilariga o'z xizmatingizni taklif qilishni istasangiz, shu yerda murojaat qoldirasiz. Biz esa sizning xizmatingizni mijozlarga taqdim etamiz.
     Buning uchun vizitka bo'limiga o'tib, o'zingiz uchun vizitka olishingiz shart
 
@@ -125,7 +135,9 @@ async def offer_function(msg: types.Message, state: FSMContext):
     Taklifingizni matn formatida yuboring 📄""",
                          reply_markup=await back_main_menu_button(msg.from_user.id))
     else:
-        await msg.answer(text="""   Здесь вы можете оставить свои предложения. 
+        await msg.answer(text="""
+    
+    Здесь вы можете оставить свои предложения. 
     Если вы хотите предложить свою услугу пользователям ботов, вы можете оставить заявку здесь. И мы предоставляем ваши услуги клиентам.
     Для этого вам необходимо зайти в раздел визитки и приобрести визитку себе.
         
@@ -156,14 +168,18 @@ Taklif:\n{msg.text}""", parse_mode='HTML')
 async def complaint_function(msg: types.Message, state: FSMContext):
     await state.set_state("complaint")
     if msg.text == complaint:
-        await msg.answer(text="""   Bu yerda siz o'z shikoyatlaringizni qoldirishingiz mumkin. 
+        await msg.answer(text="""
+    
+    Bu yerda siz o'z shikoyatlaringizni qoldirishingiz mumkin. 
     Qonun-qoidalarga amal qilmagan har qanday sotuvchi yoki xaridor darhol qora ro'yxatga kiritilib, bloklanadi. Bloklangan raqam orqali botdan qayta foydalana olmaydi
     Har bir foydalanuvchi asosli shikoyat qoldirish huquqiga ega.
 
     Shikoyatingizni matn formatida yuboring 📄""",
                          reply_markup=await back_main_menu_button(msg.from_user.id))
     else:
-        await msg.answer(text="""   Здесь вы можете оставить свои жалобы. 
+        await msg.answer(text="""
+    
+    Здесь вы можете оставить свои жалобы. 
     Любой продавец или покупатель, не соблюдающий правила, будет немедленно занесен в черный список и заблокирован. Не могу снова использовать бота из-за заблокированного номера
     Каждый пользователь имеет право подать обоснованную жалобу.
 
@@ -194,13 +210,17 @@ Shikoyat:\n{msg.text}""", parse_mode='HTML')
 async def business_card_function(msg: types.Message, state: FSMContext):
     await state.set_state("business_card")
     if msg.text == business_card:
-        await msg.answer(text="""   Botimizda o'z xizmat va mahsulotlarini taklif qiluvchi har bir a'zosi o'z vizitkasiga ega bo'lishi shart.
+        await msg.answer(text="""
+        
+    Botimizda o'z xizmat va mahsulotlarini taklif qiluvchi har bir a'zosi o'z vizitkasiga ega bo'lishi shart.
     Buning uchun pastda o'z ma'lumotlaringizni qoldirasiz va biz sizga shaxsiy tashrif qog'ozini tayyorlab beramiz
 
     Joylashuvingizni tugma orqali yuboring 👇""",
                          reply_markup=await location_buttons(msg.from_user.id))
     else:
-        await msg.answer(text="""   Каждый участник, предлагающий свои услуги и продукты с помощью нашего бота, должен иметь собственную визитную карточку.
+        await msg.answer(text="""
+        
+    Каждый участник, предлагающий свои услуги и продукты с помощью нашего бота, должен иметь собственную визитную карточку.
     Для этого вы оставляете свои данные ниже и мы подготовим для вас персональную визитку
 
     Укажите свое местоположение через кнопку 👇""",
