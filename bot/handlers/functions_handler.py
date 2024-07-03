bbed025c7805894948303942e0f06968217333c7
muhammadnur <muhammadnurpdp@gmail.com>
2024-07-03 09:29:13 +0500

    All updates

--- bot/handlers/functions_handler.py
+++ bot/handlers/functions_handler.py
@@ -299,25 +299,27 @@ Username: @{msg.from_user.username}
 Ism-Familiya: {tg_user['full_name']}
 Telefon raqam: {tg_user['phone_number']}"""
     if msg.content_type == types.ContentType.PHOTO:
-        await bot.send_photo(chat_id=directory_channel_id, photo=msg.photo[-1].file_id, caption=caption,
+        await bot.send_photo(chat_id=directory_channel_id, photo=msg.photo[-1].file_id, caption=msg.caption,
                              parse_mode='HTML')
     elif msg.content_type == types.ContentType.VIDEO:
-        await bot.send_video(chat_id=directory_channel_id, video=msg.video.file_id, caption=caption, parse_mode='HTML')
+        await bot.send_video(chat_id=directory_channel_id, video=msg.video.file_id, caption=msg.caption,
+                             parse_mode='HTML')
     elif msg.content_type == types.ContentType.DOCUMENT:
-        await bot.send_document(chat_id=directory_channel_id, document=msg.document.file_id, caption=caption,
+        await bot.send_document(chat_id=directory_channel_id, document=msg.document.file_id, caption=msg.caption,
                                 parse_mode='HTML')
     elif msg.content_type == types.ContentType.AUDIO:
-        await bot.send_audio(chat_id=directory_channel_id, audio=msg.audio.file_id, caption=caption, parse_mode='HTML')
+        await bot.send_audio(chat_id=directory_channel_id, audio=msg.audio.file_id, caption=msg.caption,
+                             parse_mode='HTML')
     elif msg.content_type == types.ContentType.VOICE:
-        await bot.send_voice(chat_id=directory_channel_id, voice=msg.voice.file_id, caption=caption, parse_mode='HTML')
+        await bot.send_voice(chat_id=directory_channel_id, voice=msg.voice.file_id, caption=msg.caption,
+                             parse_mode='HTML')
     elif msg.content_type == types.ContentType.ANIMATION:
-        await bot.send_animation(chat_id=directory_channel_id, animation=msg.animation.file_id, caption=caption,
+        await bot.send_animation(chat_id=directory_channel_id, animation=msg.animation.file_id, caption=msg.caption,
                                  parse_mode='HTML')
     elif msg.content_type == types.ContentType.STICKER:
         await bot.send_sticker(chat_id=directory_channel_id, sticker=msg.sticker.file_id)
     else:
-        await bot.send_message(chat_id=directory_channel_id, text=f"{caption}\nMa'lumot:\n{msg.text}",
-                               parse_mode='HTML')
+        await bot.send_message(chat_id=directory_channel_id, text=msg.text, parse_mode='HTML')
     if tg_user['language'] == 'uz':
         await msg.answer("Ariza yuborildi.\nTez orada aloqaga chiqamiz 😊",
                          reply_markup=await main_menu_buttons(msg.from_user.id))
